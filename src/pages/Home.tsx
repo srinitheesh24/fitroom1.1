d6ddf7a4580d17f17ad9a64401716acae712289b
srinitheesh24 <146699376+srinitheesh24@users.noreply.github.com>
2025-05-16 11:40:26 +0530

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to FitRoom</h1>
+      <p>This is the homepage</p>
+    </div>
+  );
+}
